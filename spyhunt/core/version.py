ff96a04c8e12459d8e495a126aae2aada0eb274a
MMDRZA <pymmdrza@gmail.com>
2025-11-18 23:58:05 +0300

    Add centralized version and metadata for SpyHunt

---
+++ spyhunt/core/version.py
@@ -0,0 +1,8 @@
+"""
+Centralized version and metadata for SpyHunt.
+"""
+
+SPYHUNT_NAME: str = "SpyHunt"
+SPYHUNT_VERSION: str = "4.0.0"
+SPYHUNT_AUTHOR: str = "Pymmdrza (fork from gotr00tday)"
+SPYHUNT_TAGLINE: str = "Professional Cybersecurity Reconnaissance Framework"
